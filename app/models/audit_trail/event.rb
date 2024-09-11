3a6d690bf6dc2cfd3d466a6657ecb5dbcd8c9528
Rahoul Baruah <rahoulb@echodek.co>
2024-09-11 21:48:51 +0100

    Recording multiple events
    Keeping a stack of contexts that are inherited as new events are recorded within each other

--- app/models/audit_trail/event.rb
+++ app/models/audit_trail/event.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module AuditTrail
   class Event < ApplicationRecord
     belongs_to :context, class_name: "AuditTrail::Event", optional: true
@@ -8,5 +10,42 @@ module AuditTrail
     validates :partition, presence: true
     serialize :data, type: Hash, coder: YAML, default: {}
     enum :status, ready: 0, in_progress: 10, completed: 100, failed: -1
+
+    def result
+      result_as_value || result_as_model
+    end
+
+    def result= value
+      value.is_a?(ActiveRecord::Base) ? record_result_as_model(value) : record_result_as_value(value)
+    end
+
+    def exception= value
+      data[EXCEPTION_CLASS_NAME] = value.class.name
+      data[EXCEPTION_MESSAGE] = value.message
+    end
+
+    def exception_class = data[EXCEPTION_CLASS_NAME]
+
+    def exception_message = data[EXCEPTION_MESSAGE]
+
+    private
+
+    def result_as_value = data[RESULT]
+
+    def result_as_model
+      links.find_by(name: RESULT)&.model
+    end
+
+    def record_result_as_value(value)
+      value.nil? ? data.delete(RESULT) : data[RESULT] = value
+    end
+
+    def record_result_as_model(model)
+      links.create! name: RESULT, partition: partition, model: model
+    end
+
+    RESULT = "audit_trail/event/result"
+    EXCEPTION_CLASS_NAME = "audit_trail/event/exception_class"
+    EXCEPTION_MESSAGE = "audit_trail/event/exception_message"
   end
 end
--- lib/audit_trail/recording.rb
+++ lib/audit_trail/recording.rb
@@ -1,9 +1,48 @@
 module AuditTrail
-  def self.record event_name, partition: "event", user: nil, context: nil, **params
+  def self.record event_name, partition: nil, user: nil, context: nil, **params, &block
+    context ||= current_context
+    partition ||= context&.partition || "event"
+    user ||= context&.user
+
     models = params.select { |key, value| value.is_a? ActiveRecord::Base }
     data = params.select { |key, value| !value.is_a? ActiveRecord::Base }
-    Event.create!(name: event_name, context: context, partition: partition, user: user, data: data, status: "completed").tap do |event|
-      models.each { |key, model| event.links.create! name: key, model: model, partition: partition }
+
+    Event.create!(name: event_name, context: context, partition: partition, user: user, data: data, status: "in_progress").tap do |event|
+
+      begin
+        push_context event
+
+        models.each { |key, model| event.links.create! name: key, model: model, partition: partition }
+
+        event.update result: block&.call, status: "completed"
+      rescue => ex
+        event.update status: "failed", exception: ex
+      ensure
+        pop_context
+      end
     end
+  end
+
+  def self.call_in_context event, &block
+    push_context event
+    event.result = block.call
+  ensure
+    pop_context
+  end
+
+  def self.current_context
+    context_stack.last
+  end
+
+  def self.push_context context
+    context_stack << context
+  end
+
+  def self.pop_context
+    context_stack.pop
+  end
+
+  def self.context_stack
+    Thread.current[:audit_trail_context_stack] ||= []
   end
 end
